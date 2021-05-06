59b2123e4b49752e51d6f1f4ba530c47dceaa465
dchechumaev <dmitriy.chechumaev@v6services.ru>
2021-05-06 21:21:14 +0300

    Изучаю Spring websocket

---
+++ src/main/java/dom/dima/springlearning/learningwebsocket/HelloMessage.java
@@ -0,0 +1,8 @@
+package dom.dima.springlearning.learningwebsocket;
+
+
+
+public class HelloMessage {
+    private String name;
+
+}
---
+++ src/test/java/dom/dima/springlearning/learningwebsocket/LearningWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package dom.dima.springlearning.learningwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
